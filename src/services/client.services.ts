3e46f6f9ceb1212c0e8ec594e91060f2ad6335f1
Adrison Gomez <adrisonjr.gomez@gmail.com>
2020-10-12 23:43:17 -0400

    [NEW] Creating Client and Vendor Services

---
+++ src/services/client.services.ts
@@ -0,0 +1,10 @@
+import { Client, mapFromRecordToClient } from '../models/client.model'
+import Service from './airtable'
+
+export default class ClientService extends Service {
+  constructor () {
+    super('Clients')
+  }
+
+    getAllClient = async (): Promise<Client[]> => (await this.getAllRecord()).map(mapFromRecordToClient)
+}
